085c6f05dc10fd52b25217fc345bc15d460ff251
mirsoo78 <132189912+mirsoo78@users.noreply.github.com>
2023-04-30 16:40:05 +0900

    uploading-test
    gitHub 첫 사용 테스트

---
+++ MyPlayground_01.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class Person {
+    var name : String?
+    var age : Int = 0
+    init(name:String, age:Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+var count = 0
+let names = ["소녀시대", "걸스데이", "여자친구", "티아라", "애프터스쿨"]
+let ages = Array<Int>([20, 23, 22, 20, 21])
+
+func creatPerson () -> Person? {
+    var person : Person?
+    if count < names.count {
+        person = Person(name: names[count], age: ages[count])
+    } else {
+        print("인덱스 \(count)에 사람 이름이 없습니다.")
+    }
+    
+    count += 1
+    return person
+}
+
+var person1 : Person?
+
+person1 = creatPerson()
+print("만들어진 사람 객체: \(person1!.name!)")
+
+person1 = creatPerson()
+print("만들어진 사람 객체: \(person1!.name!)")
+
+person1 = creatPerson()
+print("만들어진 사람 객체: \(person1!.name!)")
